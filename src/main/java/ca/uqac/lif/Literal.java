ebe9183dcaeb40362ffb56958f07718f90d720c1
Quentin Betti <quentin.betti@gmail.com>
2018-01-11 17:04:05 -0500

    Adding new prop

--- src/main/java/ca/uqac/lif/Literal.java
+++ src/main/java/ca/uqac/lif/Literal.java
@@ -6,10 +6,14 @@ public class Literal {
     public static final String DIR_TRACES = DIR_RESOURCES + "traces/";
 
     public static final String FILE_METHOD_NAMES = DIR_TRACES + "method_names.txt";
+    public static final String FILE_AD_LOG = DIR_TRACES + "active_directory.txt";
 
 
     public static final String METHOD_LINE_MANAGER = "org/gjt/sp/jedit/buffer/LineManager";
     public static final String METHOD_KILL_RING = "org/gjt/sp/jedit/buffer/KillRing";
     public static final String METHOD_CONTENT_MANAGER = "org/gjt/sp/jedit/buffer/ContentManager";
     public static final String METHOD_UNDO_MANAGER = "org/gjt/sp/jedit/buffer/UndoManager";
+
+    public static final String EVENT_LOGON  = "Logon";
+    public static final String EVENT_LOGOFF = "Logoff";
 }
